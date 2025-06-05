1098b0415d98a7d032e5b38e9b9f3a23004efde4
muazkadan <muaz.kadan@gmail.com>
2025-06-05 14:23:52 +0300

    Add SPM for KMP plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.vanniktech.mavenPublish)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.spmForKmp)
 }
 
 group = "dev.muazkadan"
