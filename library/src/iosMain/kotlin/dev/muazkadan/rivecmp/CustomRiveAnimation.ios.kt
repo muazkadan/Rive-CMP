b09ca31c72b9245d3e29d1097bb276e4eae1dfa7
muazkadan <muaz.kadan@gmail.com>
2025-06-05 15:32:59 +0300

    Enable Swift Package Manager and configure Rive dependency

---
+++ library/src/iosMain/kotlin/dev/muazkadan/rivecmp/CustomRiveAnimation.ios.kt
@@ -0,0 +1,12 @@
+package dev.muazkadan.rivecmp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+actual fun CustomRiveAnimation(
+    modifier: Modifier,
+    url: String,
+    stateMachineName: String?
+) {
+}
