a64473679efdb0c00cb56a342f422506f2bf9819
muazkadan <muaz.kadan@gmail.com>
2025-06-05 15:34:53 +0300

    add iOS implementation for CustomRiveAnimation

--- library/src/iosMain/kotlin/dev/muazkadan/rivecmp/CustomRiveAnimation.ios.kt
+++ library/src/iosMain/kotlin/dev/muazkadan/rivecmp/CustomRiveAnimation.ios.kt
@@ -1,12 +1,39 @@
 package dev.muazkadan.rivecmp
 
+import RiveRuntime.RiveAlignment
+import RiveRuntime.RiveFit
+import RiveRuntime.RiveViewModel
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitView
+import kotlinx.cinterop.ExperimentalForeignApi
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun CustomRiveAnimation(
     modifier: Modifier,
     url: String,
     stateMachineName: String?
 ) {
+    val viewModel = remember {
+        RiveViewModel(
+            webURL = url,
+            stateMachineName = stateMachineName,
+            fit = RiveFit.contain,
+            autoPlay = true,
+            alignment = RiveAlignment.center,
+            loadCdn = false,
+            artboardName = null
+        )
+    }
+    UIKitView(
+        factory = {
+            viewModel.createRiveView()
+        },
+        modifier = modifier,
+        update = { view ->
+            viewModel.updateWithView(view)
+        }
+    )
 }
