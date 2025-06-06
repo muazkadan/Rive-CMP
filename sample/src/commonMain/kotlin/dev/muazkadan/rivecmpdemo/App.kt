3470f50f6c4641b9f810da3852547868fe8462fa
muazkadan <muaz.kadan@gmail.com>
2025-06-06 18:59:54 +0300

    add animation from resources example to sample project

--- sample/src/commonMain/kotlin/dev/muazkadan/rivecmpdemo/App.kt
+++ sample/src/commonMain/kotlin/dev/muazkadan/rivecmpdemo/App.kt
@@ -8,12 +8,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.muazkadan.rivecmp.CustomRiveAnimation
+import dev.muazkadan.rivecmp.utils.ExperimentalRiveCmpApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import rivecmp.sample.generated.resources.Res
 
+@OptIn(ExperimentalRiveCmpApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
+        var resourceAnimation by remember { mutableStateOf<ByteArray?>(null) }
+        LaunchedEffect(Unit){
+            resourceAnimation = Res.readBytes("files/mode_switch.riv")
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxSize(),
@@ -23,6 +31,12 @@ fun App() {
                 modifier = Modifier.fillMaxWidth().weight(1f),
                 url = "https://cdn.rive.app/animations/off_road_car_v7.riv"
             )
+            resourceAnimation?.let {
+                CustomRiveAnimation(
+                    modifier = Modifier.fillMaxWidth().weight(1f),
+                    byteArray = it
+                )
+            }
         }
     }
 }
