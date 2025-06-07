4b8a8290aabef18c934de41b6fb44dbce2dba734
muazkadan <muaz.kadan@gmail.com>
2025-06-07 13:01:29 +0300

    Updated the sample app to demonstrate usage of the new rememberRiveComposition and RiveCompositionSpec.

--- sample/src/commonMain/kotlin/dev/muazkadan/rivecmpdemo/App.kt
+++ sample/src/commonMain/kotlin/dev/muazkadan/rivecmpdemo/App.kt
@@ -8,6 +8,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.muazkadan.rivecmp.CustomRiveAnimation
+import dev.muazkadan.rivecmp.RiveCompositionSpec
+import dev.muazkadan.rivecmp.rememberRiveComposition
 import dev.muazkadan.rivecmp.utils.ExperimentalRiveCmpApi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import rivecmp.sample.generated.resources.Res
@@ -17,9 +19,16 @@ import rivecmp.sample.generated.resources.Res
 @Preview
 fun App() {
     MaterialTheme {
-        var resourceAnimation by remember { mutableStateOf<ByteArray?>(null) }
-        LaunchedEffect(Unit){
-            resourceAnimation = Res.readBytes("files/mode_switch.riv")
+        val urlAnimation by rememberRiveComposition {
+            RiveCompositionSpec.url(
+                url = "https://cdn.rive.app/animations/off_road_car_v7.riv"
+            )
+        }
+
+        val resourceAnimation by rememberRiveComposition {
+            RiveCompositionSpec.byteArray(
+                byteArray = Res.readBytes("files/mode_switch.riv")
+            )
         }
 
         Column(
@@ -29,14 +38,12 @@ fun App() {
         ) {
             CustomRiveAnimation(
                 modifier = Modifier.fillMaxWidth().weight(1f),
-                url = "https://cdn.rive.app/animations/off_road_car_v7.riv"
+                composition = urlAnimation
             )
-            resourceAnimation?.let {
-                CustomRiveAnimation(
-                    modifier = Modifier.fillMaxWidth().weight(1f),
-                    byteArray = it
-                )
-            }
+            CustomRiveAnimation(
+                modifier = Modifier.fillMaxWidth().weight(1f),
+                composition = resourceAnimation
+            )
         }
     }
 }
