6633e2c6ad49e3ceca99fadb5c8f69862ed200e4
muazkadan <muaz.kadan@gmail.com>
2025-12-21 12:46:49 +0300

    Update rive-ios dependency version to 6.12.3

--- library/exportedNativeIosShared/Package.swift
+++ library/exportedNativeIosShared/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.9
 
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "exportedNativeIosShared",
-    platforms: [.iOS("14.0"), .macOS("10.13"), .tvOS("12.0"), .watchOS("4.0")],
+    platforms: [.iOS("14.0"),.macOS("10.13"),.tvOS("12.0"),.watchOS("4.0")],
     products: [
         .library(
             name: "exportedNativeIosShared",
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["exportedNativeIosShared"])
     ],
     dependencies: [
-        .package(url: "https://github.com/rive-app/rive-ios.git", exact: "6.11.1")
+        .package(url: "https://github.com/rive-app/rive-ios.git", exact: "6.12.3")
     ],
     targets: [
         .target(
@@ -21,9 +21,9 @@ let package = Package(
                 .product(name: "RiveRuntime", package: "rive-ios")
             ],
             path: "Sources"
-
+            
         )
-
+        
     ]
 )
         
